47630b64b941a5ee5404ce54f279cc7b418f22b8
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-07-10 19:11:43 -0400

    Update

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
-app.use(express.static('HackathonNotification'));
+app.use(express.static('VandyHacksNotification'));
