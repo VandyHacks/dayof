3e40d4de0a13a806a71a9dbe5dad28cfa414cffe
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-07-10 19:41:49 -0400

    Update

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const twilio = require('twilio');
-const app = express();
+const uri = process.env.PROD_MONGODB;
 const PORT = process.env.PORT || 5000;
+const app = express();
 
 app.use(helmet());
 app.use(express.static('VandyHacksNotification'));
+
+mongoose.connect(uri);
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+var client = new twilio(process.env.TWILIO_TEST_SID, process.env.TWILIO_TEST_AUTH);
+
