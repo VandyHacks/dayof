e1acdd367e26063f2b4359953cdcb352567ba3da
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-07-10 21:25:28 -0400

    Connecting to database

--- index.js
+++ index.js
@@ -14,5 +14,22 @@ mongoose.Promise = global.Promise;
 
 var db = mongoose.connection;
 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+  console.log("Database open");
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/form.html");
+  console.log("Page loaded");
+})
+
 var client = new twilio(process.env.TWILIO_TEST_SID, process.env.TWILIO_TEST_AUTH);
 
+app.post('/', (req, res) => {
+  var text = req.body;
+})
+
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
+})
