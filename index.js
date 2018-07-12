4fb22611f5ea5e05f3967524af3200c766a78be7
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-07-11 23:45:15 -0400

    Update

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 
 app.post('/message', (req, res) => {
   for (let i=0; i<phoneArr.length; i++) {
-    txtbelt.sendText("4074809635", req.body);
+    txtbelt.sendText("407-480-9635", req.body);
   }
 })
 
