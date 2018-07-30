0f7bd3fdc6f62969337230538850280e77dc3e01
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-07-30 17:54:06 -0400

    Debug

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ app.get('/', cors(), (req, res) => {
 });
 
 app.post('/', (req, res) => {
+  console.log('Checkpoint 1');
   Hacker.find({}, (err, data) => {
     if (err) throw err;
     data.forEach((element) => {
@@ -77,6 +78,7 @@ app.post('/', (req, res) => {
       }
     });
   });
+  console.log('Checkpoint 2');
   Promise.all(
     phoneArr.map(number => twilio.messages.create({
       to: number,
