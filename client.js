b3f1192076747f493ed840354ea4abede455bde1
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-04 03:06:22 -0400

    Debug

--- client.js
+++ client.js
@@ -55,7 +55,7 @@ async function run() {
   // Register Service Worker
   console.log('Registering service worker');
   const registration = await navigator.serviceWorker
-    .register('/worker.js', { scope: '/' })
+    .register('./worker.js', { scope: '/' })
     .catch((err) => {
       console.log(err);
     });
