bec86fa60c7374f66ad34fa4c4083315a54a6538
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-05 23:58:54 -0400

    Add confirm

--- client.js
+++ client.js
@@ -62,16 +62,23 @@ async function run() {
   // Send Push Notification
   console.log('Sending push');
   console.log('PushSubscription: ', JSON.stringify(subscription));
-  document.getElementById('pushnotif').onclick = await fetch('/dayof', {
-    method: 'POST',
-    body: JSON.stringify({
-      subscribe: subscription,
-      timetolive: ttl,
-    }),
-    headers: {
-      'Content-type': 'application/json',
-    },
-  });
+  document.getElementById('pushnotif').onclick = () => {
+    if (document.getElementById('msg').value !== '') {
+      if (window.confirm('Send message?')) {
+        alert('Messages sent!');
+        fetch('/dayof', {
+          method: 'POST',
+          body: JSON.stringify({
+            subscribe: subscription,
+            timetolive: ttl,
+          }),
+          headers: {
+            'Content-type': 'application/json',
+          },
+        });
+      }
+    }
+  };
   console.log('Sent push');
 }
 
