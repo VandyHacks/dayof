21c27e687aa5ab965d0e7163f272a1203be92656
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-05 23:53:45 -0400

    Debug

--- worker.js
+++ worker.js
@@ -3,9 +3,7 @@ console.log('Loaded service worker!');
 self.addEventListener('push', (ev) => { // eslint-disable-line no-restricted-globals
   const data = ev.data.json();
   console.log('Got push', data);
-  ev.waitUntil(
-    self.registration.showNotification(data.title, { // eslint-disable-line no-restricted-globals
-      body: data.body,
-    }),
-  );
+  self.registration.showNotification(data.title, { // eslint-disable-line no-restricted-globals
+    body: data.body,
+  });
 });
