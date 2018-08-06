e9882c85b858795964ebe94f5ea4b29c980832a0
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-06 19:10:03 -0400

    Debug

--- index.js
+++ index.js
@@ -114,12 +114,16 @@ const isValidSaveRequest = (req, res) => {
 function saveSubscriptionToDatabase(subscription) {
   return new Promise((resolve, reject) => {
     console.log('Saving subscription to database');
-    ds.insert(subscription, (err, newDoc) => {
-      if (err) {
-        console.log('Error occurred');
-        reject(err);
+    ds.find(subscription, (error, doc) => {
+      if (doc === undefined || doc.length === 0) {
+        ds.insert(subscription, (err, newDoc) => {
+          if (err) {
+            console.log('Error occurred');
+            reject(err);
+          }
+          resolve(newDoc._id); // eslint-disable-line no-underscore-dangle
+        });
       }
-      resolve(newDoc._id); // eslint-disable-line no-underscore-dangle
     });
     ds.find({}, (err, docs) => {
       console.log(docs);
