c4e59a6b2737346c3b0a170d355092fe9ba72661
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-07 02:28:38 -0400

    Debug

--- index.js
+++ index.js
@@ -143,6 +143,7 @@ app.post('/savesub', (req, res) => {
 // Dayof route
 app.post('/dayof', (req, res) => {
   // Resource created successfully
+  console.log(res);
   const payload = JSON.stringify({ title: 'VandyHacks', body: message });
   // const sub = req.body.subscribe;
   const options = {
