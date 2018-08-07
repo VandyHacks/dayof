26a8d4247b015b2f80e42cfb4fe6c0fbce76d798
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-07 18:27:22 -0400

    Debug

--- index.js
+++ index.js
@@ -106,7 +106,13 @@ const isValidSaveRequest = (req, res) => {
 
 app.post('/savesub', (req, res) => {
   if (isValidSaveRequest) {
-    const push = new PushSub(req.body);
+    const push = new PushSub({
+      endpoint: req.body.endpoint,
+      keys: {
+        p256dh: req.body.p256dh,
+        auth: req.body.auth,
+      },
+    });
     console.log(req.body);
     console.log(push);
     console.log('Saving subscription to database');
