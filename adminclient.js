ac45e8a8acdba46d6830dd746baa82c9a233873c
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-09 22:08:43 -0400

    Debug

--- adminclient.js
+++ adminclient.js
@@ -1,7 +1,7 @@
 console.log('Accessed client.js');
 
-sessionStorage.setItem('msgvalue', '');
-sessionStorage.setItem('submitted', false);
+localStorage.setItem('msgvalue', '');
+localStorage.setItem('submitted', false);
 
 const publicKey = 'BLG1-QasBcbWCAShq_GBT-H_Dmb4gdR3pjUyBhzHYNrPjkoJcQgwHut_D3MGL0c6mbM3BPreabClVFMGPQHx9h0';
 
