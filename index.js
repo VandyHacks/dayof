5f170033129f4c89e19a72ec5ee460994a3702e9
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-11 18:49:55 -0400

    Debug

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.post('/savesub', (req, res) => {
 app.post('/sendpush', (req, res) => {
   // Resource created successfully
   console.log(message); // added
-  const payload = JSON.stringify({ title: 'VandyHacks', body: message }); // eslint-disable-line changed message to req.body
+  const payload = JSON.stringify({ title: 'VandyHacks', body: req.body }); // eslint-disable-line changed message to req.body
   // const sub = req.body.subscribe;
   const options = {
     TTL: ttl,
