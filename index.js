d4bf3e784bdbdbee8ab7fd9b7e40fd04c96944b7
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-11 18:51:27 -0400

    Debug

--- index.js
+++ index.js
@@ -140,8 +140,8 @@ app.post('/savesub', (req, res) => {
 // Dayof route
 app.post('/sendpush', (req, res) => {
   // Resource created successfully
-  console.log(message); // added
-  const payload = JSON.stringify({ title: 'VandyHacks', body: req.body }); // eslint-disable-line changed message to req.body
+  console.log(req.body); // added
+  const payload = JSON.stringify({ title: 'VandyHacks', body: JSON.stringify(req.body) }); // eslint-disable-line changed message to req.body
   // const sub = req.body.subscribe;
   const options = {
     TTL: ttl,
