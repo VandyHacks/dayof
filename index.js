ced50bae83a0a29ebcbb4e664aafcb58e58b431b
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-11 22:44:41 -0400

    Rework

--- index.js
+++ index.js
@@ -5,23 +5,21 @@ const parser = require('body-parser');
 const cors = require('cors');
 const twilio = require('twilio')(process.env.TWILIO_LIVE_SID, process.env.TWILIO_LIVE_AUTH);
 const webpush = require('web-push');
-// const Datastore = require('nedb');
+const Push = require('./schemas/schemas').pushSchema;
+const Hack = require('./schemas/schemas').hackerSchema;
 
 const uri = process.env.PROD_MONGODB;
 const PORT = process.env.PORT || 5000;
 const publicVapidKey = process.env.WEBPUSH_PUBLIC;
 const privateVapidKey = process.env.WEBPUSH_PRIVATE;
-// const ds = new Datastore();
+const ttl = 600;
 const app = express();
-
-module.exports = publicVapidKey;
 
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 
 app.use(helmet());
 app.use(express.static(__dirname));
-app.use(express.static(`${__dirname}/client`));
 
 app.use(cors());
 
@@ -32,7 +30,7 @@ webpush.setVapidDetails(
   privateVapidKey,
 );
 
-mongoose.connect(uri);
+mongoose.connect(uri, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
@@ -44,16 +42,8 @@ db.once('open', () => {
 
 const phoneArr = [];
 
-let message;
-
-const hackerSchema = new mongoose.Schema({
-  firstName: { type: String, max: 20 },
-  lastName: { type: String, max: 20 },
-  school: { type: String, max: 50 },
-  email: { type: String, max: 100 },
-  phone: { type: String, max: 15 },
-});
-const Hacker = db.model('Hacker', hackerSchema);
+const Hacker = db.model('Hacker', Hack);
+const PushSub = db.model('PushSubscription', Push);
 
 function dbquery(callback) {
   Hacker.find({}, (err, data) => {
@@ -77,21 +67,26 @@ function wait() {
 
 dbquery(wait);
 
-app.get('/', cors(), (req, res) => {
-  res.sendFile(`${__dirname}/form.html`);
-  console.log('Page loaded');
+app.get('/dayof', (req, res) => {
+  res.sendFile(`${__dirname}/live.html`);
+  console.log('Live notifications page loaded');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/admin.html`);
+  console.log('Admin page loaded');
 });
 
 app.post('/', (req, res) => {
   Promise.all(
-    message = req.body.msg,
     phoneArr.map(number => twilio.messages.create({
       to: number,
       from: process.env.TWILIO_MASS_SMS_SID,
-      body: `VandyHacks: ${message}`,
+      body: `VandyHacks: ${req.body.msg}`,
     })),
   )
     .then(
+      console.log('Message sent'),
       res.redirect('back'),
     )
     .catch((err) => {
@@ -100,17 +95,68 @@ app.post('/', (req, res) => {
     });
 });
 
-// Dayof route
-app.post('/dayof', (req, res) => {
-  // Resource created successfully
-  const payload = JSON.stringify({ title: 'VandyHacks', body: message });
-  const sub = req.body.subscription;
-  console.log(sub);
-  webpush.sendNotification(sub, payload)
-    .then(res.sendStatus(201))
-    .catch((err) => {
-      console.log(err.stack);
+function isValidSaveRequest(req, res) {
+  // Check for endpoint
+  if (!req.body || !req.body.endpoint) {
+    // Not valid subscription
+    res.status(400);
+    console.log('Subscription must have endpoint');
+    return false;
+  }
+  return true;
+}
+
+function exists(subscription) {
+  return PushSub.countDocuments({ endpoint: subscription.endpoint, key: subscription.key });
+}
+
+app.post('/savesub', (req, res) => {
+  if (isValidSaveRequest(req, res)) {
+    const push = new PushSub(req.body);
+    exists(push).then((count) => {
+      if (count === 0) {
+        console.log('Saving subscription to database');
+        push.save()
+          .then(() => {
+            res.setHeader('Content-type', 'application/json');
+            res.sendStatus(201);
+            console.log('Push subscription saved');
+          })
+          .catch((err) => {
+            console.log('Unable to save push subscription', err);
+          });
+      } else {
+        console.log('Subscription already exists in database');
+        res.sendStatus(201);
+      }
     });
+  }
+});
+
+// Dayof route
+app.post('/sendpush', (req, res) => {
+  // Resource created successfully
+  console.log(req.body); // added
+  const payload = JSON.stringify({ title: 'VandyHacks', body: req.body.value }); // eslint-disable-line changed message to req.body
+  const options = {
+    TTL: ttl,
+  };
+  console.log(payload);
+  PushSub.find({}, (err, data) => {
+    if (err) throw err;
+    Promise.all(
+      data.forEach((element) => {
+        webpush.sendNotification(element, payload, options);
+      }),
+    )
+      .then(
+        console.log('Push notification sent'),
+        res.sendStatus(201),
+      )
+      .catch((error) => {
+        console.log(error.stack);
+      });
+  });
 });
 
 app.listen(PORT, () => {
