c57021c2e955cc1bbb983e0533879b2f22de11fb
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-08-12 00:58:50 -0400

    Debug

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const parser = require('body-parser');
 const cors = require('cors');
 const twilio = require('twilio')(process.env.TWILIO_LIVE_SID, process.env.TWILIO_LIVE_AUTH);
 const webpush = require('web-push');
+const WebSocket = require('ws');
 const Push = require('./schemas/schemas').pushSchema;
 const Hack = require('./schemas/schemas').hackerSchema;
 
 const uri = process.env.PROD_MONGODB;
+const PORT = process.env.PORT || 5000;
 const publicVapidKey = process.env.WEBPUSH_PUBLIC;
 const privateVapidKey = process.env.WEBPUSH_PRIVATE;
 const ttl = 600;
-const app = express();
+const app = express()
+  .use((req, res) => res.sendFile(`${__dirname}/live.html`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
@@ -21,6 +25,12 @@ app.use(helmet());
 app.use(express.static(__dirname));
 
 app.use(cors());
+
+const wss = new WebSocket.Server({ app });
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
 
 webpush.setGCMAPIKey(process.env.GCM_KEY);
 webpush.setVapidDetails(
