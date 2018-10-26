77f8ad558a342aafe2a97d461b9fc9f1082a6b70
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-10-26 11:07:50 -0500

    Revert

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cors());
 
 webpush.setGCMAPIKey(process.env.GCM_KEY);
 webpush.setVapidDetails(
-  'mailto:dev@vandyhacks.org', // change to environment variable
+  'mailto:dev@vandyhacks.org',
   publicVapidKey,
   privateVapidKey,
 );
