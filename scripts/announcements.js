76093fd08bc4ecd4cf9d698db2d9056026d91067
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-10-27 15:57:32 -0500

    relative time

--- scripts/announcements.js
+++ scripts/announcements.js
@@ -1,5 +1,3 @@
-import Moment from 'react-moment';
-
 const HOST = window.location.origin.replace(/^https/, 'wss');
 const ws = new WebSocket(HOST);
 const container = document.getElementById('announcements-col');
