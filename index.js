dadbccd078e136b4f0521458cc71d66ba9c41fbe
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-10-29 22:17:16 -0500

    Debug notifications

--- index.js
+++ index.js
@@ -250,7 +250,8 @@ app.post('/sendpush', (req, res) => {
 app.post('/updatemsg', (req, res) => {
   const promise = new Promise((resolve, reject) => {
     // add most recent notification to json
-    Message.findOne().sort({ created_at: -1 }).exec((err, docs) => {
+    // Message.findOne().sort({ created_at: -1 }).exec((err, docs) => {
+    Message.find({}, (err, docs) => {
       if (err) reject(err);
       wss.clients.forEach((client) => {
         if (client === connect) {
