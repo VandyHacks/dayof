e0215d31cb9e74ccfeda1e9c2223f1865f835323
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-11-01 19:51:52 -0500

    add schemas

---
+++ schemas/schemas.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const pushSchema = new mongoose.Schema({
+  endpoint: String,
+  keys: {
+    p256dh: String,
+    auth: String,
+  },
+});
+
+const hackerSchema = new mongoose.Schema({
+  firstName: { type: String, max: 20 },
+  lastName: { type: String, max: 20 },
+  school: { type: String, max: 50 },
+  email: { type: String, max: 100 },
+  phone: { type: String, max: 15 },
+});
+
+const msgSchema = new mongoose.Schema({
+  header: String,
+  msg: String,
+  time: Date,
+});
+
+module.exports = { pushSchema, hackerSchema, msgSchema };
